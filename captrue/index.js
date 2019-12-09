5d37bfc119262beb5bb61ee64a8d3027399a5099
tian <tianlu.tian@zhaopin.com.cn>
2019-12-09 23:01:44 +0800

    ok

---
+++ captrue/index.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const url = require('url')
+const ejs = require('ejs')
+const client = require('./src/client')
+
+
+http.createServer(function(req, res) {
+  let pathname = url.parse(req.url, true).pathname
+  res.writeHead(200, {
+    "Content-Type": "text/html;charset='utf-8"
+  })
+  // console.log(client())
+  client(function(data){
+    ejs.renderFile('./views/zhihu.ejs', {
+      title: 'zhihu',
+      imgList: data
+    }, function(err, data) {
+      if(err){
+        console.log(err)
+        return false
+      }
+      res.write(data)
+      res.end()
+    })
+  })
+
+}).listen(3000, function(err){
+  if(err){
+    console.log(err)
+    return false
+  }
+  console.log('http://localhost:3000')
+})
